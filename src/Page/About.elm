67f5af48897cfcf71e1d66e1619f79d55f7ef094
Steven Bapaga <sbapaga@gmail.com>
2019-08-24 03:24:12 +0100

    Update page styles to be mobile friendly

--- src/Page/About.elm
+++ src/Page/About.elm
@@ -32,7 +32,7 @@ view : Model -> Html Msg
 view model =
     div [ class "o-page" ]
         [ div [ class "pure-g" ]
-            [ div [ class "pure-u-2-3" ]
+            [ div [ class "pure-u-1 pure-u-md-2-3" ]
                 [ h3 [ class "c-heading-alpha" ] [ text "About Find me" ]
                 , p [] [ text "I started writting this game because I wanted to get some practice using elm." ]
                 , p [] [ text "The benefits of writting a game is that you can take it as far as you want" ]
--- src/Page/Game.elm
+++ src/Page/Game.elm
@@ -573,7 +573,7 @@ view : Model -> Html Msg
 view model =
     div [ class "o-page" ]
         [ div [ class "pure-g" ]
-            [ div [ class "pure-u-2-3" ]
+            [ div [ class "pure-u-1 pure-u-md-2-3 u-order-2" ]
                 [ div [ class "c-game-container" ]
                     [ viewStart model
                     , div [ class "pure-g" ]
@@ -587,7 +587,7 @@ view model =
                     , viewGameOver model.gameStatus
                     ]
                 ]
-            , div [ class "pure-u-1-3" ]
+            , div [ class "pure-u-1 pure-u-md-1-3" ]
                 [ viewSidebar model ]
             ]
         ]
