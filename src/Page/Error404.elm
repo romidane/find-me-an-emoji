359d2ff35c4612bc9e23148cf156a110fe8bc04c
Steven Bapaga <sbapaga@gmail.com>
2019-08-24 19:32:00 +0100

    Update 404 page layout

--- src/Page/Error404.elm
+++ src/Page/Error404.elm
@@ -1,8 +1,18 @@
 module Page.Error404 exposing (view)
 
-import Html exposing (Html, div, text)
+import Html exposing (Html, a, div, h1, text)
+import Html.Attributes exposing (class, href)
 
 
 view : Html msg
 view =
-    div [] [ text "The page you are looking for does not exit" ]
+    div [ class "o-page" ]
+        [ div [ class "pure-g" ]
+            [ div
+                [ class "pure-u-1 pure-u-md-3-4" ]
+                [ h1 [ class "c-heading-alpha" ]
+                    [ text "The page you are looking for does not exist" ]
+                , a [ href "/" ] [ text "Return home" ]
+                ]
+            ]
+        ]
