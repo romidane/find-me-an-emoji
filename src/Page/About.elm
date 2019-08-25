f9ab304343c195b876aceaf4064f18eb3b5c9018
Steven Bapaga <sbapaga@gmail.com>
2019-08-25 22:19:26 +0100

    Use lorem batman for about page

--- src/Page/About.elm
+++ src/Page/About.elm
@@ -10,7 +10,7 @@ import Html.Attributes exposing (..)
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { title = "Likedy split" }
+    ( { title = "About" }
     , Cmd.none
     )
 
@@ -33,9 +33,15 @@ view model =
     div [ class "o-page" ]
         [ div [ class "pure-g" ]
             [ div [ class "pure-u-1 pure-u-md-2-3" ]
-                [ h3 [ class "c-heading-alpha" ] [ text "About Find me" ]
-                , p [] [ text "I started writting this game because I wanted to get some practice using elm." ]
-                , p [] [ text "I wanted to experience how hard it would be write a single page application as well as do something that would invlove me using a lot of the Api's elm has to offer" ]
+                [ h3 [ class "c-heading-alpha" ]
+                    [ text "Some Batman Ipsum" ]
+                , p
+                    []
+                    [ text "Blue knight superman diamond shiva basil ragdoll czonk mask, bane scorn. Firebug oracle shade green faith charlatan checkmate czonk moth bane. Clayface alcor ragman rose rupert oracle. Hammer poison scarecrow martha deathstroke dick supergirl nyssa. Aquaman aquaman joker gotham thorne batmobile jason echo cluemaster kyle scarecrow zeus. Green blake arrow deathstroke edward harlequin wayne pennyworth a pit harvey abbott? Mugsy hush grundy al calendar boomerang? Falcone gearhead joe." ]
+                , p []
+                    [ text "Todd penguin caird lantern selina barrow elongated flash fairchild poison elongated! Gotham nyssa, arrow batman edward. Gearhead temblor oracle ragman. Wing bane america blink anarky atomic! Raatko ragdoll, bruce wayne rhino a barrow kobra. Of atom lynx moth checkmate ivy, clock young young mad canary martha? Harlequin vale ragman smoke fairchild. Black maxie a lazarus! America chase dent thomas montoya! Zeus joker." ]
+                , p []
+                    [ text "Young aquaman arrow green black shadow. Black night wing kane calendar aquaman smoke temblor anarky. Face prey riddler luthor grey superman clayface aquaman. Tumbler diamond oswald blake swamp chill crane rhino azrael arrow rose. Kobra, diamond firebug gearhead snake ra smoke. Pit zeus clock harley jim riddler grey blink. Drake crane toymaker barbara spoiler thomas clench sinestro! Boomerang riddler hangman cain snake basil, freeze pennyworth batgirl hood sinestro gleeson. Superman grey lex society damian thorne killer robin? Faith shiva mad clock deathstroke!" ]
                 ]
             ]
         ]
